81b8474b3a2755470f73af3417bc15b23182b232
lsefiane <lyes.sefiane@gmail.com>
2023-03-03 12:06:11 -0500

    Implement Interfaces in TypeScript

--- typescript-essential-training/module03-Implement_interfaces_in_TypeScript.ts
+++ typescript-essential-training/module03-Implement_interfaces_in_TypeScript.ts
@@ -31,7 +31,7 @@ interface IceCream {
 }
 
 // Extends an Interface
-// 
+// Able to extends multiple interfaces
 interface Sundae extends IceCream {
     sauce: 'chocolate' | 'caramel' | 'strawberry';
     nuts?: boolean;
@@ -57,3 +57,35 @@ function tooManyScoops(dessert: Sundae) {
     }
 }
 console.log(tooManyScoops({flavor: 'vanilla', scoops: 5, sauce: 'caramel'}));
+
+// ===================
+
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts'
+
+// Interface describing the shape of our json data
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function fetchPosts(url: string) {
+    let response = await fetch(url);
+    let body = await response.json();
+    return body as Post[];
+}
+
+async function showPost() {
+    let posts = await fetchPosts(fetchURL);
+    for (const post of posts) {
+        console.log('Post #' + post.id)
+        // If the userId is 1, then display a note that it's an administrator
+        console.log('Author: ' + (post.userId === 1 ? "Administrator" : post.userId.toString()))
+        console.log('Title: ' + post.title)
+        console.log('Body: ' + post.body)   
+    }
+}
+
+showPost();
